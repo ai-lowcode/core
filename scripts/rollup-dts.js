30170cf89bb651f4f464ebb55cb26821312252fc
axleu <axelulue@gmail.com>
2024-08-01 13:38:19 +0800

    chore: 🚀 新增编译打包脚本，优化 package json 数据

--- scripts/rollup-dts.js
+++ scripts/rollup-dts.js
@@ -43,11 +43,10 @@ async function run() {
   }
   else {
     console.error(
-      `🚨类型声明文件生成失败：${
-             +`\n\t${extractorResult.errorCount} errors`
-             }\n\tand ${extractorResult.warningCount} warnings`,
+          `🚨类型声明文件生成失败：${
+              +`\n\t${extractorResult.errorCount} errors`
+          }\n\tand ${extractorResult.warningCount} warnings`,
     )
-    await rimraf(join(libPath, 'temp'))
     exit(1)
   }
 }

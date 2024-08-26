3c1208d24ce1a5e1ca14477ebead2dbad0f8723e
axleu <axelulue@gmail.com>
2024-08-26 23:16:25 +0800

    build: 📦‍ 修改打包路径

--- app/designer/vite.config.ts
+++ app/designer/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
